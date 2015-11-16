2a221a43e031f75d1ecd7c689944e5e4daca2b44
Lazar Stanojević <lazar@laxhosting.rs>
2015-11-16 19:37:42 +0100

    Aktuelni nazivi elemenata tokom učitavanja

--- js/piro.js
+++ js/piro.js
@@ -2,12 +2,12 @@
 document.onreadystatechange = function () {
 	var state = document.readyState
 	if (state == 'interactive') {
-		document.getElementById('tst').style.visibility="hidden";
+		document.getElementById('kontejner').style.visibility="hidden";
 	} else if (state == 'complete') {
 		setTimeout(function() {
 			document.getElementById('interactive');
 			document.getElementById('load').style.visibility="hidden";
-			document.getElementById('tst').style.visibility="visible";
+			document.getElementById('kontejner').style.visibility="visible";
 		}, 1000);
 	}
 }
