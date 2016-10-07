9260be90b6852f8bbdfb45b19331e18efa055de9
Lazar Stanojević <lazar.stanojevic@pmf.edu.rs>
2016-10-08 01:47:23 +0200

    Manje izmene

--- rpi/autotemp.sh
+++ rpi/autotemp.sh
@@ -3,4 +3,4 @@
 # ukoliko je pec postavljena na automatski rezim i izvrsava se u 8 sati ujutru.
 # 0 8 * * * /bin/bash /var/www/html/piro/rpi/autotemp.sh
 echo "Izvrsavanje autotemp skripte..."
-wget -q -O - "http://localhost/piro/rpi/piro-querry.php?f=autoTemp&arg=-1"
+wget -q -O - "http://localhost/piro/rpi/piro-query.php?f=autoTemp&arg=-1"
